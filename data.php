5a942fac2b85fda5c27d87b1300e561ebc5c7540
Evgeny Kunavin <kunavinjenya55@gmail.com>
2023-04-10 16:05:10 +0000

    Update data.php

--- data.php
+++ data.php
@@ -13,11 +13,3 @@ $message = $data['message']['text']; // Текст сообщения
 $message_id = $data['message']['message_id']; // ID сообщения
 $first_name = $data['message']['from']['first_name'];
 $last_name = $data['message']['from']['last_name'];
-
-$inline_query = $data['inline_query']; // Inline Query
-$inline_query_id = $data['inline_query']['id']; // ID Inline - запроса
-$inline_query_msg = $data['inline_query']['query']; // Сообщение Inline - запроса (сам запрос)
-$inline_user_id = $data['inline_query']['from']['id']; // ID юзера Inline - запроса
-$inline_username = $data['inline_query']['from']['username']; // Юзернейм того, кто делает Inline - запрос
-$inline_first_name = $data['inline_query']['from']['first_name']; // Имя того, кто делает Inline - запрос
-$inline_chat_type = $data['inline_query']['chat_type']; // Тип чата
